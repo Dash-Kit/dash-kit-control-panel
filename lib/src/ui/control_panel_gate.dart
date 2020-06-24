0e0084f77e3e89a43a2cd8b5b52b6a7e5395ffde
Artem Abramov <artem.abramov@dualbootpartners.com>
2020-06-24 15:24:28 +0300

    Implemented a property for enabling / disabling gate

--- lib/src/ui/control_panel_gate.dart
+++ lib/src/ui/control_panel_gate.dart
@@ -2,32 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:flutter_platform_control_panel/src/panel/control_panel.dart';
 
 class ControlPanelGate extends StatelessWidget {
-  const ControlPanelGate({Key key, @required this.child}) : super(key: key);
+  const ControlPanelGate({
+    Key key,
+    @required this.child,
+    this.isEnabled = true,
+  }) : super(key: key);
 
   static DateTime _lastDoubleTapEvent;
 
   final Widget child;
+  final bool isEnabled;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       behavior: HitTestBehavior.translucent,
-      onDoubleTap: () {
-        final now = DateTime.now();
-        final diff = _lastDoubleTapEvent != null
-            ? now.millisecondsSinceEpoch -
-                _lastDoubleTapEvent.millisecondsSinceEpoch
-            : -1;
-
-        if (_lastDoubleTapEvent != null && diff >= 0 && diff <= 2500) {
-          _lastDoubleTapEvent = null;
-
-          ControlPanel.open();
-        }
-
-        _lastDoubleTapEvent = now;
-      },
+      onDoubleTap: isEnabled ? _onDoubleTap : null,
       child: child,
     );
+  }
+
+  void _onDoubleTap() {
+    final now = DateTime.now();
+    final diff = _lastDoubleTapEvent != null
+        ? now.millisecondsSinceEpoch -
+            _lastDoubleTapEvent.millisecondsSinceEpoch
+        : -1;
+
+    if (_lastDoubleTapEvent != null && diff >= 0 && diff <= 2500) {
+      _lastDoubleTapEvent = null;
+
+      ControlPanel.open();
+    }
+
+    _lastDoubleTapEvent = now;
   }
 }
