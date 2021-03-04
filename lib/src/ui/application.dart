4850f3d1e8316f1cc7c824149ee0c7bc5bfa462e
Artem Abramov <artem.abramov@dualbootpartners.com>
2021-03-04 20:41:39 +0400

    Removed device preview support

--- lib/src/ui/application.dart
+++ lib/src/ui/application.dart
@@ -1,6 +1,4 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:dash_kit_control_panel/src/services/device_preview_mode.dart';
 
 class Application extends StatelessWidget {
   const Application({required this.child});
@@ -9,18 +7,13 @@ class Application extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: DevicePreviewMode.onModeChanged,
-      builder: (context, snapShoot) => DevicePreview(
-          enabled: snapShoot.hasData && snapShoot.data,
-          builder: (context) => child),
-    );
+    return child;
   }
 
   static Widget appBuilder(
     BuildContext context,
     Widget widget,
   ) {
-    return DevicePreview.appBuilder(context, widget);
+    return widget;
   }
 }
